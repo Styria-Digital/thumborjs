121108572a210966a7b4a3290958dd3af0277f7c
Ivan Katanovic <ivan.katanovic@styria.hr>
2020-11-20 18:40:21 +0100

    Use browser crypto api

--- index.js
+++ index.js
@@ -1,379 +1,289 @@
-// core deps
-const crypto    = require('crypto');
-
-// locals
-const keys      = {};
-const consts    = {
-    TOP    : 'top',
-    MIDDLE : 'middle',
-    BOTTOM : 'bottom',
-
-    RIGHT  : 'right',
-    CENTER : 'center',
-    LEFT   : 'left',
-};
-
 /**
- * returns whether given input a non-empty string
- *
- * @param  {String} s
- * @return {Boolean}
+ * @param {[type]} securityKey
+ * @param {[type]} thumborServerUrl
  */
-function validString(s)
-{
-    return (typeof s === 'string') && s.length;
+function Thumbor(securityKey, thumborServerUrl) {
+  'use strict';
+
+  this.THUMBOR_SECURITY_KEY = securityKey;
+  this.THUMBOR_URL_SERVER = thumborServerUrl;
+
+  this.imagePath = '';
+  this.width = 0;
+  this.height = 0;
+  this.smart = false;
+  this.fitInFlag = false;
+  this.withFlipHorizontally = false;
+  this.withFlipVertically = false;
+  this.halignValue = null;
+  this.valignValue = null;
+  this.cropValues = null;
+  this.meta = false;
+  this.filtersCalls = [];
 }
 
-function initSeckey(seckey)
-{
-    if (!validString(seckey))
-        return null;
+Thumbor.prototype = {
 
-    const existing_key  = Object.getOwnPropertySymbols(keys).reduce((pv, sym) => pv || (keys[sym] === seckey ? sym : null), null);
+  TOP: 'top',
+  MIDDLE: 'middle',
+  BOTTOM: 'bottom',
 
-    if (existing_key)
-        return existing_key;
+  RIGHT: 'right',
+  CENTER: 'center',
+  LEFT: 'left',
 
-    const new_key       = Symbol();
+  /**
+   * Set path of image
+   * @param {String} imagePath [description]
+   */
+  setImagePath: function(imagePath) {
+    this.imagePath = (imagePath.charAt(0) === '/') ?
+      imagePath.substring(1, imagePath.length) : imagePath;
+    return this;
+  },
+  /**
+   * Converts operation array to string
+   * @return {String}
+   */
+  getOperationPath: function() {
+    var parts = this.urlParts();
 
-    keys[ new_key ] = seckey;
+    if (0 === parts.length) {
+      return '';
+    }
 
-    return new_key;
-}
+    return parts.join('/') + '/';
+  },
+  /**
+   * Build operation array
+   *
+   * @TODO Should be refactored so that strings are generated in the
+   * commands as opposed to in 1 massive function
+   *
+   * @return {Array}
+   */
+  urlParts: function() {
+    if (!this.imagePath) {
+      throw new Error('The image url can\'t be null or empty.');
+    }
 
-function getSeckey(prop)
-{
-    return prop && keys[ prop ];
-}
+    var parts = [];
 
+    if (this.meta) {
+      parts.push('meta');
+    }
 
-/**
- * Build operation path, must be bound to instance of Thumbor
- *
- * @return {Array}
- */
-function getOperationPath(reset)
-{
-    if (!this.imagePath)
-        throw new Error('The image url can\'t be null or empty (call `imgpath()` first).');
+    if (this.cropValues) {
+      parts.push(
+        this.cropValues.left +
+        'x' + this.cropValues.top +
+        ':' + this.cropValues.right +
+        'x' + this.cropValues.bottom
+      );
+    }
 
-    const parts = [];
+    if (this.fitInFlag) {
+      parts.push('fit-in');
+    }
 
-    if (this.meta)
-        parts.push('meta');
-
-    if (this.cropValues)
-        parts.push(
-            this.cropValues.left +
-            'x' + this.cropValues.top +
-            ':' + this.cropValues.right +
-            'x' + this.cropValues.bottom
-        );
-
-    if (this.fitInFlag)
-        parts.push('fit-in');
 
     if (
-        this.width                ||
-        this.height               ||
-        this.withFlipHorizontally ||
-        this.withFlipVertically
+      this.width ||
+      this.height ||
+      this.withFlipHorizontally ||
+      this.withFlipVertically
     ) {
-        let sizeString = '';
+      var sizeString = '';
 
-        if (this.withFlipHorizontally)
-            sizeString += '-';
+      if (this.withFlipHorizontally) {
+        sizeString += '-';
+      }
+      sizeString += this.width;
 
-        sizeString += this.width;
-        sizeString += 'x';
+      sizeString += 'x';
 
-        if (this.withFlipVertically)
-            sizeString += '-';
+      if (this.withFlipVertically) {
+        sizeString += '-';
+      }
+      sizeString += this.height;
 
-        sizeString += this.height;
-
-        parts.push(sizeString);
+      parts.push(sizeString);
     }
 
-    if (this.halignValue)
-        parts.push(this.halignValue);
-
-
-    if (this.valignValue)
-        parts.push(this.valignValue);
-
-    if (this.smart)
-        parts.push('smart');
-
-    if (this.filtersCalls.length)
-        parts.push('filters:' + this.filtersCalls.join(':'));
-
-    parts.push(this.imagePath);
-
-    if (reset)
-        this.reset();
-
-    return parts.join('/');
-}
-
-class ThumborJS
-{
-    /**
-     * ctor
-     *
-     * @param {String}      thumborServerUrl
-     * @param {String|null} securityKey
-     */
-    constructor(thumborServerUrl, securityKey)
-    {
-        if (!validString(thumborServerUrl))
-            throw new Error('`thumborServerUrl` parameter must be a non-empty string');
-
-        this.seckeyProp = initSeckey(securityKey);
-        this.serverUrl  = thumborServerUrl;
-
-        this.reset();
+    if (this.halignValue) {
+      parts.push(this.halignValue);
     }
 
-    /**
-     * reset the internal properties (that map to thumbor url-parts)
-     */
-    reset()
-    {
-        this.imagePath            = '';
-        this.width                = 0;
-        this.height               = 0;
-        this.smart                = false;
-        this.fitInFlag            = false;
-        this.withFlipHorizontally = false;
-        this.withFlipVertically   = false;
-        this.halignValue          = null;
-        this.valignValue          = null;
-        this.cropValues           = null;
-        this.meta                 = false;
-        this.filtersCalls         = [];
+    if (this.valignValue) {
+      parts.push(this.valignValue);
     }
 
-   /**
-    * Set path of image
-    *
-    * @param {String} imagePath
-    */
-    imgpath(imagePath, reset)
-    {
-        if (reset)
-            this.reset();
-
-        this.imagePath = (imagePath.charAt(0) === '/') ? imagePath.substring(1, imagePath.length) : imagePath;
-
-        return this;
+    if (this.smart) {
+      parts.push('smart');
     }
 
-    /**
-     * Resize the image to the specified dimensions. Overrides any previous call
-     * to `fitIn` or `resize`.
-     *
-     * Use a value of 0 for proportional resizing. E.g. for a 640 x 480 image,
-     * `.resize(320, 0)` yields a 320 x 240 thumbnail.
-     *
-     * Use a value of 'orig' to use an original image dimension. E.g. for a 640
-     * x 480 image, `.resize(320, 'orig')` yields a 320 x 480 thumbnail.
-     * @param  {String} width
-     * @param  {String} height
-     */
-    resize(width, height)
-    {
-        this.width  = width  || '';
-        this.height = height || '';
-
-        return this;
+    if (this.filtersCalls.length) {
+      parts.push('filters:' + this.filtersCalls.join(':'));
     }
 
-    /**
-     * turn on smart-crop
-     */
-    smartcrop(smartCrop)
-    {
-        this.smart = smartCrop;
+    return parts;
+  },
+  /**
+   * Resize the image to the specified dimensions. Overrides any previous call
+   * to `fitIn` or `resize`.
+   *
+   * Use a value of 0 for proportional resizing. E.g. for a 640 x 480 image,
+   * `.resize(320, 0)` yields a 320 x 240 thumbnail.
+   *
+   * Use a value of 'orig' to use an original image dimension. E.g. for a 640
+   * x 480 image, `.resize(320, 'orig')` yields a 320 x 480 thumbnail.
+   * @param  {String} width
+   * @param  {String} height
+   */
+  resize: function(width, height) {
+    this.width = width;
+    this.height = height;
+    this.fitInFlag = false;
+    return this;
+  },
 
-        return this;
+  smartCrop: function(smartCrop) {
+    this.smart = smartCrop;
+    return this;
+  },
+  /**
+   * Resize the image to fit in a box of the specified dimensions. Overrides
+   * any previous call to `fitIn` or `resize`.
+   *
+   * @param  {String} width
+   * @param  {String} height
+   */
+  fitIn: function(width, height) {
+    this.width = width;
+    this.height = height;
+    this.fitInFlag = true;
+    return this;
+  },
+  /**
+   * Flip image horizontally
+   */
+  flipHorizontally: function() {
+    this.withFlipHorizontally = true;
+    return this;
+  },
+  /**
+   * Flip image vertically
+   */
+  flipVertically: function() {
+    this.withFlipVertically = true;
+    return this;
+  },
+  /**
+   * Specify horizontal alignment used if width is altered due to cropping
+   * @param  {String} halign 'left', 'center', 'right'
+   */
+  halign: function(halign) {
+    if (
+      halign === this.LEFT ||
+      halign === this.RIGHT ||
+      halign === this.CENTER
+    ) {
+      this.halignValue = halign;
+    } else {
+      throw new Error('Horizontal align must be left, right or center.');
     }
-
-    /**
-     * Resize the image to fit in a box of the specified dimensions. Overrides
-     * any previous call to `fitIn` or `resize`.
-     *
-     * @param  {String} width
-     * @param  {String} height
-     */
-    fitin(width, height)
-    {
-        this.fitInFlag = true;
-
-        return this.resize(width, height);
+    return this;
+  },
+  /**
+   * Specify vertical alignment used if height is altered due to cropping
+   * @param  {String} valign 'top', 'middle', 'bottom'
+   */
+  valign: function(valign) {
+    if (
+      valign === this.TOP ||
+      valign === this.BOTTOM ||
+      valign === this.MIDDLE
+    ) {
+      this.valignValue = valign;
+    } else {
+      throw new Error('Vertical align must be top, bottom or middle.');
     }
+    return this;
+  },
+  /**
+   * Specify that JSON metadata should be returned instead of the thumbnailed
+   * image.
+   * @param  {Boolean} metaDataOnly [description]
+   */
+  metaDataOnly: function(metaDataOnly) {
+    this.meta = metaDataOnly;
+    return this;
+  },
+  /**
+   * Append a filter, e.g. quality(80)
+   * @param  {String} filterCall
+   */
+  filter: function(filterCall) {
+    this.filtersCalls.push(filterCall);
+    return this;
+  },
+  /**
+   * Manually specify crop window.
+   * @param  {Integer} left
+   * @param  {Integer} top
+   * @param  {Integer} right
+   * @param  {Integer} bottom
+   * @return {[type]}
+   */
+  crop: function(left, top, right, bottom) {
+    this.cropValues = {
+      left: left,
+      top: top,
+      right: right,
+      bottom: bottom
+    };
 
-    /**
-     * Flip image horizontally
-     */
-    hflip()
-    {
-        this.withFlipHorizontally = true;
+    return this;
+  },
+  /**
+   * Combine image url and operations with secure and unsecure (unsafe) paths
+   * @return {String}
+   */
+  buildUrl: async function() {
+    var operation = this.getOperationPath();
 
-        return this;
+    if (this.THUMBOR_SECURITY_KEY) {
+      var enc = new TextEncoder("utf-8");
+
+      var cryptoKey = await window.crypto.subtle.importKey(
+        "raw",
+        enc.encode(this.THUMBOR_SECURITY_KEY),
+        {
+            name: "HMAC",
+            hash: {name: "SHA-1"}
+        },
+        false,
+        ["sign", "verify"]
+      );
+      var signature = await window.crypto.subtle.sign(
+        "HMAC",
+        cryptoKey,
+        enc.encode(operation + this.imagePath)
+      );
+      var hashArray = Array.from(new Uint8Array(signature));
+      var hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+      var base64 = btoa(hashHex.match(/\w{2}/g).map(function(a){return String.fromCharCode(parseInt(a, 16));} ).join(""))
+      var key = base64.replace(/\+/g, '-').replace(/\//g, '_');
+
+      return this.THUMBOR_URL_SERVER +
+          '/' + key +
+          '/' + operation +
+          this.imagePath;
+
+    } else {
+      return this.THUMBOR_URL_SERVER + '/unsafe/' + operation + this.imagePath;
     }
-
-    /**
-     * Flip image vertically
-     */
-    vflip()
-    {
-        this.withFlipVertically = true;
-
-        return this;
-    }
-
-    /**
-     * Specify horizontal alignment used if width is altered due to cropping
-     *
-     * @param  {String} halign 'left', 'center', 'right'
-     */
-    halign(halign)
-    {
-        if (
-          halign !== consts.LEFT &&
-          halign !== consts.RIGHT &&
-          halign !== consts.CENTER
-        ) throw new Error(`Horizontal align must be ${consts.LEFT}, ${consts.RIGHT} or ${consts.CENTER}.`);
-
-        this.halignValue = halign;
-
-        return this;
-    }
-
-    /**
-     * Specify vertical alignment used if height is altered due to cropping
-     *
-     * @param  {String} valign 'top', 'middle', 'bottom'
-     */
-    valign(valign)
-    {
-        if (
-          valign !== consts.TOP &&
-          valign !== consts.BOTTOM &&
-          valign !== consts.MIDDLE
-        ) throw new Error(`Vertical align must be ${consts.TOP}, ${consts.BOTTOM} or ${consts.MIDDLE}.`);
-
-        this.valignValue = valign;
-
-        return this;
-    }
-
-    /**
-     * Specify that JSON metadata should be returned instead of the thumbnailed image.
-     *
-     * @param  {Boolean} metaData
-     */
-    metadata(metaData)
-    {
-        this.meta = !!metaData;
-
-        return this;
-    }
-
-    /**
-     * Append a filter, e.g. quality(80)
-     * @param  {String} filterCall
-     */
-    filter(filterCall)
-    {
-        if (this.filtersCalls.indexOf(filterCall) == -1)
-            this.filtersCalls.push(filterCall);
-
-        return this;
-    }
-
-    /**
-     * Manually specify crop window.
-     *
-     * @param  {Integer} left
-     * @param  {Integer} top
-     * @param  {Integer} right
-     * @param  {Integer} bottom
-     * @return {[type]}
-     */
-    crop(left, top, right, bottom)
-    {
-        this.cropValues = {
-            left   : left,
-            top    : top,
-            right  : right,
-            bottom : bottom
-        };
-
-        return this;
-    }
-
-    /**
-     * generate and return the "operation path" based on the "operation" settings currently defined in this ThumborJS instance
-     *
-     * N.B. this function is primarily for testing purposes - it allows for taking the "operation path" and
-     *      performing the encryption with Thumbor's own python module ... thereby allowing for verfication of
-     *      the generated security hash
-     *
-     * @return {String}
-     * @throws {Error}      - thrown if an "image path" is not currently set
-     */
-    getpath()
-    {
-        return getOperationPath.call(this);
-    }
-
-    /**
-     * generate and return the encryption hash part of a thumbor URL,
-     * based on the "operation" settings currently defined in this ThumborJS instance,
-     * or the explicit `operation` string (if given)
-     *
-     * @param  {String} operation       - optional, will auto-generate it if not supplied
-     * @return {String}
-     */
-    gethash(operation)
-    {
-        if (!this.seckeyProp)
-            return 'unsafe'; // no security key set
-
-        return crypto.createHmac('sha1', getSeckey(this.seckeyProp))
-                     .update(operation || this.getpath())
-                     .digest('base64')
-                     .replace(/\+/g, '-')
-                     .replace(/\//g, '_')
-                     ;
-
-    }
-
-    /**
-     * generate a thumbor image url
-     *
-     * @param  {Boolean} skip_reset     - optional, pass TRUE to skip resetting of objects internal state (after generation of "operation path")
-     * @param  {Boolean} skip_serverurl - optional, pass TRUE to skip prefixing of generated URL with "server url"
-     * @return {String}
-     * @throws {Error}                  - thrown if an "image path" is not currently set
-     */
-    genurl(skip_reset, skip_serverurl)
-    {
-        const operation = getOperationPath.call(this, !skip_reset);
-
-        return (skip_serverurl ? '' : this.serverUrl) + '/' + this.gethash(operation) + '/' + operation;
-    }
+  }
 };
 
-// expose "class constants"
-Object.keys(consts).forEach(k => Object.defineProperty(ThumborJS, k, {
-    configurable : false,
-    writable     : false,
-    value        : consts[k],
-
-}));
-
-// export class
-module.exports  = ThumborJS;
+module.exports = Thumbor;
